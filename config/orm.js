f267ccb620016d35365d2b8232130b743bef58a7
Alex Khoa Le <leduykhoa@hotmail.ca>
2019-11-09 18:20:19 -0500

    Completed functions to interact with database

--- config/orm.js
+++ config/orm.js
@@ -2,14 +2,28 @@
 var connection = require("../config/connection.js");
 
 var orm = {
-    selectAll: () => {
-
+    selectAll: (table, cb) => {
+        var queryString = `SELECT * FROM ${table};`;
+        connection.query(queryString, function(err, res) {
+          if (err) {
+            throw err;
+          }
+          cb(res);
+        });
     },
-    insertOne: () => {
-
+    insertOne: (table, cols, vals, cb) => {
+        let queryString = `INSERT INTO ${table} (${cols}) VALUES (${vals});`;
+        connection.query(queryString, function(err, res) {
+            if (err) throw err;
+            cb(res);
+        })
     },
-    updateOne: () => {
-
+    updateOne: (table, colNewVal, condition, cb) => {
+        let queryString = `UPDATE ${table} SET ${colNewVal} WHERE ${condition};`;
+        connection.query(queryString, (err, res) => {
+            if (err) throw err;
+            cb(res);
+        })
     }
 }
 
