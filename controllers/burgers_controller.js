eab8f5e3b90b2711b5eb3e7f1ef4937798600fb7
Alex Khoa Le <leduykhoa@hotmail.ca>
2019-11-09 20:59:00 -0500

    Adjusted parameters passed into insertOne function

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -16,7 +16,8 @@ router.get("/", function (req, res) {
 });
 
 router.post("/api/burgers", function (req, res) {
-    burger.insertOne("burger_name", req.body.name, function (result) {
+    console.log(req);
+    burger.insertOne(req.body.name, function (result) {
         res.json({ id: result.insertId });
     })
 });
