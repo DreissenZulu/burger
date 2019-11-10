0a4a5903142eae27101482cbcb13febed9fd7f06
Alex Khoa Le <leduykhoa@hotmail.ca>
2019-11-10 00:09:56 -0500

    Fixed bug in put command

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -16,17 +16,16 @@ router.get("/", function (req, res) {
 });
 
 router.post("/api/burgers", function (req, res) {
-    console.log(req);
     burger.insertOne(req.body.name, function (result) {
         res.json({ id: result.insertId });
     })
 });
 
 router.put("/api/burgers/:id", function (req, res) {
-    let condition = `id = ${req.params.id}`;
+    let condition = `id=${req.params.id}`;
     console.log(`Condition: ${condition}`);
 
-    burger.updateOne("devoured = true;", condition, function (result) {
+    burger.updateOne("devoured=true", condition, function (result) {
         if (result.changedRows === 0) {
             // If no rows were changed, then the ID must not exist, so 404
             return res.status(404).end();
